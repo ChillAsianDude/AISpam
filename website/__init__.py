b643db7b15ffbae9bffe0a2a2088f813445a4394
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-21 16:20:31 +0800

    updated files

--- website/__init__.py
+++ website/__init__.py
@@ -1,27 +1,48 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
-import os
+from flask_login import LoginManager
 
+# database initialisation
 db = SQLAlchemy ()
+DB_NAME = "database.db"
 
 def create_app ():
-    app = Flask (__name__)
-    app.config['SECRET_KEY'] = 'BC3415'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    # externel database link for render
-    # format (username, password, host, database name)
-    # postgresql://ai_spam_user:SnDrgfmQFOOTEBhCtTrPKPOgS5ByEEnA@dpg-crudpkggph6c73agm96g-a.oregon-postgres.render.com/ai_spam
+    app = Flask(__name__)
+    app.config ['SECRET_KEY'] = 'BC3415'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
+    # import views from different apps
+    from .auth.start import auth_views
+    from .pages.home import home_page
+    from .pages.forum import forum_page
+    from .pages.rewards import rewards_page
+    from .pages.explore import explore_page
+    from .pages.learning import learning_page
+    from .pages.profile import profile_page
 
-    # internal database link for render
-    # postgresql://ai_spam_user:SnDrgfmQFOOTEBhCtTrPKPOgS5ByEEnA@dpg-crudpkggph6c73agm96g-a/ai_spam
+    # redirecting the user for when they are not logged in to their account
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth_views.login'
+    login_manager.init_app(app)
 
-    db.init_app (app)
+    app.register_blueprint (auth_views, url_prefix='/')
+    app.register_blueprint (home_page, url_prefix='/')
+    app.register_blueprint (forum_page, url_prefix='/')
+    app.register_blueprint (rewards_page, url_prefix='/')
+    app.register_blueprint (explore_page, url_prefix='/')
+    app.register_blueprint (learning_page, url_prefix='/')
+    app.register_blueprint (profile_page, url_prefix='/')
 
-    from .views import views 
-    from .auth import auth
+    from .models import User
 
-    app.register_blueprint (views, url_prefix='/')
-    app.register_blueprint (auth, url_prefix='/')
+    with app.app_context():
+        db.create_all()
+
+    @login_manager.user_loader
+    def load_user (id):
+        return User.query.get (int(id))
 
     return app
+
+    
