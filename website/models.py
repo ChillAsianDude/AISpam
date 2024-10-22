63bbe631f39e863c42a83b3e44e5cc5ee9d39f12
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-22 16:55:53 +0800

    changes

--- website/models.py
+++ website/models.py
@@ -2,9 +2,9 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note (db.Model):
+class Results (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # notes data, string max  = 100000 characters
+    # results data, string max  = 100000 characters
     data = db.Column(db.String (100000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     # one-to-many relationship with User table
@@ -17,4 +17,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String (150), unique=True)
     password = db.Column(db.String (150))
     first_name = db.Column(db.String (150))
-    notes = db.relationship('Note')
+    results = db.relationship('Results')
--- website/pages/explore.py
+++ website/pages/explore.py
@@ -18,28 +18,31 @@ def explore_result ():
         # receiving data
         text_data = request.form.get('text_data')
         speech_data = request.form.get ('speech_data')
-        audio_data = request.files ['audio_data']
-
-        #convert audio data to text
-        recognizer = sr.Recognizer()
-        audioFile = sr.AudioFile (audio_data)
-        with audioFile as source:
-            data = recognizer.record(source)
-        audio_data = recognizer.recognize_google (data, key=None)
-        # maybe can use google genai, would require api key to be kept in environment
+        audio_data = request.files ['audio_data']           
 
         # append data to dictionary
-        data_received ["text"] = text_data
-        data_received ["speech"] = speech_data
-        data_received ["audio"] = audio_data
-    
+        if text_data != "":
+            data_received ["text"] = text_data
+        
+        if audio_data.filename != "":
+            # convert audio to text
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile (audio_data)
+            with audioFile as source:
+                data = recognizer.record(source)
+            audio_data = recognizer.recognize_google (data, key=None)
+            data_received ["audio"] = audio_data
+            # maybe can use google genai, would require api key to be kept in environment
+        
+        if speech_data != "":
+            data_received ["speech"] = speech_data
+        
     # analysing data
     analysis_data = {}
     for data, content in data_received.items ():
         if content == None or content != "":
             analysis = textblob.TextBlob (content).sentiment
             analysis_data [data] = {"polarity": analysis.polarity, "subjectivity": analysis.subjectivity}
-
         
     return render_template ("explore_result.html",user=current_user,
                             data_received=data_received, 
